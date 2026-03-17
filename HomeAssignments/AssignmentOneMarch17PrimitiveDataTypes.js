5703d0265337d84ff330c8a85e6329f72dda8eb9
saravanakumar1377 <42279059+saravanakumar1377@users.noreply.github.com>
2026-03-17 16:48:19 +0530

    Add files via upload
    Primitive Data Type

---
+++ HomeAssignments/AssignmentOneMarch17PrimitiveDataTypes.js
@@ -0,0 +1,19 @@
+let userName = "Testleaf";
+console.log(userName);
+console.log(typeof userName);
+console.log("-----------------------------");
+let organization = "Qeagle";
+console.log(organization);
+console.log(typeof organization);
+console.log("-----------------------------");
+let contactNumber = 979114567
+console.log(contactNumber);
+console.log(typeof contactNumber);
+console.log("-----------------------------"); 
+let knowsAutomation = true
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+console.log("-----------------------------"); 
+let usesPlaywright
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright);
