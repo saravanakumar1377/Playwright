2a9755e39224bdee4cea9544021a72de71eee4e8
saravanakumar1377 <42279059+saravanakumar1377@users.noreply.github.com>
2026-03-17 18:40:41 +0530

    Update AssignmentOneMarch17PrimitiveDataTypes.js

--- HomeAssignments/AssignmentOneMarch17PrimitiveDataTypes.js
+++ HomeAssignments/AssignmentOneMarch17PrimitiveDataTypes.js
@@ -1,19 +1,24 @@
+//string
 let userName = "Testleaf";
 console.log(userName);
 console.log("typeof:" + typeof userName);
 console.log("-----------------------------");
+//string
 let organization = "Qeagle";
 console.log(organization);
 console.log("typeof:" + typeof organization);
 console.log("-----------------------------");
+//number
 let contactNumber = 979114567
 console.log(contactNumber);
 console.log("typeof:" + typeof contactNumber);
 console.log("-----------------------------"); 
+//boolean
 let knowsAutomation = true
 console.log(knowsAutomation);
 console.log("typeof:" + typeof knowsAutomation);
 console.log("-----------------------------"); 
+//undefineda
 let usesPlaywright
 console.log(usesPlaywright);
 console.log("typeof:" + typeof usesPlaywright);
